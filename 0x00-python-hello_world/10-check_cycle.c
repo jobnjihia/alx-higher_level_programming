e4fa42e4f6563aff22e3397388d12b19a3041c13
jobnjihia <jobnjihia09@gmail.com>
2023-08-12 00:02:18 +0000

    lists

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - examines for a cycle in a linked list
+ * @list: examined linked list
+ * Return: 0 if no cycle, else 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *snail =list;
+	listint_t *hare =list;
+
+	if(!list)
+		return(0);
+
+	while(slow&&fast&&fast->next)
+	{
+		slow
+}
