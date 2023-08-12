5e7fc4b938f6ff98cf2b0e87000b13646ef78062
jobnjihia <jobnjihia09@gmail.com>
2023-08-12 23:30:53 +0000

    addition module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if _name_ == "_main_":
-    """print the sum of a and b."""
     from add_0 import add
 
     a = 1
     b = 2
+    c = add(a, b)
 
-    print("{} + {} = {}".format(a, b, add(a+b)))
+    print("{} + {} = {}".format(a, b, c))
