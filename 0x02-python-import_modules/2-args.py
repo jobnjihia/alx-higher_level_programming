5460cf42a1bafbf1fd7e048d439879b5e7ad6712
jobnjihia <jobnjihia09@gmail.com>
2023-08-13 06:36:11 +0000

    prints and counts arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    import sys
+
+    argslen = len(sys.argv)
+
+    if argslen == 1:
+        print("arguements.".format(argslen - 1))
+    elif argslen == 2:
+        print("arguement.".format(argslen - 1))
+    else:
+        print("{}arguments:".format(arglen - 1))
+    for i in range(1, argslen):
+        print("{}: {}".format(i, sys.argv[i]))
