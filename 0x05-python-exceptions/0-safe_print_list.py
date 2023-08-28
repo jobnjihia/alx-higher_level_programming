327dbdff71dcaecfad20274de316889a7b85c898
jobnjihia <jobnjihia09@gmail.com>
2023-08-28 20:35:32 +0000

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            j = j + 1
+
+    except (IndexError, ValueError):
+        pass
+
+    print()
+    return (j)
