7c1eadb3aa71533c2ec81aa00e2f760ca4f6fdd3
jobnjihia <jobnjihia09@gmail.com>
2023-08-29 13:26:10 +0000

    python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    tokeo = 0
     try:
-        tokeo = a / b
-        return (tokeo)
+        result = a / b
+        return (result)
     except (ZeroDivisionError):
         result = None
     finally:
-        print("Inside result: {}".format(tokeo))
-        return (tokeo)
+        print("Inside result: {}".format(result))
+        return (result)
