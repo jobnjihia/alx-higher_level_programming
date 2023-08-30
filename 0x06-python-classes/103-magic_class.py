4559518344cdd7b808cf17c99026d73cd33dbc6f
jobnjihia <jobnjihia09@gmail.com>
2023-08-30 02:44:26 +0000

    python classes

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a class MagicClass that computes
+the circumfrence and area of a circle"""
+import math
+
+
+class MagicClass:
+    """defines class MagicClass"""
+    def __init__(self, radius=0):
+        """Initializes MagicClass"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Calculates the area of the circle"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Calculates the circumference of the circle"""
+        return 2 * math.pi * self.__radius
