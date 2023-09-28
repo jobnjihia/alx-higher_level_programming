91953576839c6087376180e04a0292db585d6a76
jobnjihia <jobnjihia09@gmail.com>
2023-09-28 15:32:50 +0000

    the circle

---
+++ 0x0C-python-almost_a_circle/tests/tests_base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines unittests for base.py
+Unittest classes:
+    TestBase_instantiation
+    TestBase_to_json_string
+    TestBase_save_to_file
+    TestBase_from_json_string
+    TestBase_create
+    TestBase_load_from_file
+    TestBase_save_to_file_csv
+    TestBase_load_from_file_csv
+"""
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+
